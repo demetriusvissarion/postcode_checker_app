56dc25f67f97a0b22289813ea7948b13d2e5c302
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2023-12-07 22:15:18 +0000

    fixed regex error

--- lib/postcode_checker.py
+++ lib/postcode_checker.py
@@ -4,7 +4,11 @@ class PostcodeChecker():
     def check(self, postcode):
         if postcode is None:
             return False
+        stripped_postcode = postcode.replace(" ", "")
+        # print('stripped_postcode: ' + '[start]' + stripped_postcode + '[end]')
         return re.match(
-            r"^[a-z]\d[a-z\d]?\s*\d[a-z]{2}$",
-            postcode,
+            r"^[a-z]{1,2}\d[a-z\d]?\s*\d[a-z]{2}$",
+            stripped_postcode,
             re.IGNORECASE) is not None
+        #     r"^[a-z]\d[a-z\d]?\s*\d[a-z]{2}$",
+        #     r"/^[a-z]{1,2}\d[a-z\d]?\s*\d[a-z]{2}$/i",
